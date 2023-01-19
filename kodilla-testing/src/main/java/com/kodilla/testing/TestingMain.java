600fc109447b4c2f5692caee88b2aac12217907e
Kacper <kacper0724@poczta.fm>
2023-01-19 17:28:01 +0100

    Added test SimpleUser and Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,37 @@
 package com.kodilla.testing;
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania.");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if(result.equals("theForumUser")) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        Calculator calculator = new Calculator();
+
+        int sumAdd = calculator.add(5, 55);
+
+        if(sumAdd == 60) {
+            System.out.println("Add test OK");
+        } else {
+            System.out.println("Add test error!");
+        }
+
+        int sumSubstract = calculator.substract(55, 5);
+
+        if(sumSubstract == 50) {
+            System.out.println("Substract test OK");
+        } else {
+            System.out.println("Substract test error!");
+        }
     }
 }
