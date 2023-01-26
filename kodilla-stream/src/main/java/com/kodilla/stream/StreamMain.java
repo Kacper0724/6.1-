6b47613bd64a63c65f5b618527fdc67868d43592
Kacper <kacper0724@poczta.fm>
2023-01-26 18:19:57 +0100

    Added immutable package with noimmutable class

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -31,6 +31,7 @@ public class StreamMain {
         poemBeautifier.beautify("example text", text -> text + "...");
 
         //NumbersGenerator
+        System.out.println();
 
         System.out.println("Using Stream to generate even numbers from 1 to 20");
         NumbersGenerator.generateEven(20);
