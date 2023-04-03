6db2e25d6f61ca9634dbca796ee2d85c1826bb88
Kacper <kacper0724@poczta.fm>
2023-04-03 11:19:52 +0200

    Added kodilla-jdbc module

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -45,6 +45,31 @@ public class DbManagerTestSuite {
         DbManager dbManager = DbManager.getInstance();
 
         //When
+        String sqlQuery = "SELECT U.FIRSTNAME, U.LASTNAME, I.SUMMARY\n" +
+                "  FROM USERS U\n" +
+                "  JOIN ISSUES I ON U.ID = I.USER_ID_ASSIGNEDTO";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while (rs.next()) {
+            System.out.println(rs.getString("FIRSTNAME") + ", " +
+                    rs.getString("LASTNAME") + ", " +
+                    rs.getString("SUMMARY"));
+            counter++;
+        }
+        rs.close();
+        statement.close();
+        assertEquals(15, counter);
+    }
+
+    @Test
+    void testSelectUsersAndPosts() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+
+        //When
         String sqlQuery = "SELECT U.FIRSTNAME, U.LASTNAME, COUNT(*)\n" +
                 "  FROM USERS U JOIN POSTS P ON U.ID = P.USER_ID\n" +
                 "  GROUP BY P.USER_ID\n" +
